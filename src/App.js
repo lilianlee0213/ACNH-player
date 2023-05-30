445d9a289d5b5ad9c922906c5d75fa01c3d67bbe
Lilian Lee <lilianlee0213@gmail.com>
2023-05-30 19:04:50 -0400

    add Discover route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Home from './Routes/Home';
 import Player from './Player';
+import Discover from './Routes/Discover';
 // import Wave from './components/Wave';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Home />}></Route>
+				<Route path="/discover" element={<Discover />}></Route>
 				<Route path="/play" element={<Player />}></Route>
 			</Routes>
 		</BrowserRouter>
