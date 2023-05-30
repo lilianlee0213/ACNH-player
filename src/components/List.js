55692a048104f5b0a736b32cdfde7c10c0c5c83d
Lilian Lee <lilianlee0213@gmail.com>
2023-05-30 18:04:54 -0400

    list container styles

--- src/components/List.js
+++ src/components/List.js
@@ -3,20 +3,43 @@ import styled from 'styled-components';
 const Container = styled(motion.div)`
 	position: absolute;
 	width: 290px;
+	left: 0;
+	bottom: 0px;
 	border-radius: 15px;
 	background-color: ${(props) => props.theme.yellow};
-	left: 0;
 	z-index: 2;
+	overflow-y: hidden;
+`;
+const ListContainer = styled(motion.div)`
+	display: grid;
+	grid-template-rows: repeat(5, 1fr);
+	gap: 10px;
+	margin: 20px;
+`;
+const ListItem = styled(motion.div)`
+	display: flex;
+	align-items: center;
+	gap: 10px;
+	padding: 12px;
+	border-radius: 10px;
+	background-color: ${(props) => props.theme.beige};
+`;
+const ListTitle = styled.h3`
+	font-size: 18px;
+	color: ${(props) => props.theme.black};
+`;
+const ListImage = styled.img`
+	width: 50px;
+	height: 50px;
+	box-shadow: 0px 15px 35px -5px rgba(0, 0, 0, 0.35);
 `;
 const containerVariants = {
 	show: {
-		height: '508px',
-		top: 0,
+		height: '100%',
 		transition: {duration: 0.5},
 	},
 	hide: {
 		height: 0,
-		top: '100%',
 		transition: {duration: 0.5},
 	},
 };
@@ -26,14 +49,14 @@ export default function List(props) {
 			showList={props.showList}
 			variants={containerVariants}
 			animate={props.showList ? 'show' : 'hide'}>
-			<div>
+			<ListContainer>
 				{props.songList.map((list) => (
-					<div key={list.id}>
-						<h1>{list.name['name-USen']}</h1>
-						<img src={list.image_uri} alt="" />
-					</div>
+					<ListItem key={list.id} whileHover={{scale: 1.05}}>
+						<ListImage src={list.image_uri} alt="" />
+						<ListTitle>{list.name['name-USen']}</ListTitle>
+					</ListItem>
 				))}
-			</div>
+			</ListContainer>
 		</Container>
 	);
 }
