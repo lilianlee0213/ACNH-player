75e764fa6e7cdcb3f770be56302abfee4454dc06
Lilian Lee <lilianlee0213@gmail.com>
2023-06-27 01:13:23 -0400

    test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './common-styles.css';
 
 function App() {
 	return (
-		<BrowserRouter>
+		<BrowserRouter basename={process.env.PUBLIC_URL}>
 			<Routes>
 				<Route path="/" element={<Home />}></Route>
 				<Route path="/discover" element={<Discover />}></Route>
